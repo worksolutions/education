609e94959740f958ed4d8da999a679c6e9d6e4d7
Максим Соколовский <sokolovsky@worksolutions.ru>
2016-10-12 16:35:22 +0300

    Unit 1. Task2. Серверная часть задачи.

--- Unit 1/Task 2/server.php
+++ Unit 1/Task 2/server.php
@@ -1,10 +1,23 @@
 <?php
 
+use WS\Education\Unit1\Task2\Connection;
 use WS\Education\Unit1\Task2\Server;
+
+include __DIR__."/bootstrap.php";
 
 $server = new Server(10100);
 
-$file = fopen(__DIR__.'/data/file.txt', 'r');
-$server->registerHandler(function ($connection) use ($file) {
-
+$fileStrings = file(__DIR__.'/data/file.txt');
+$server->registerHandler(function (Connection $connection) use ($fileStrings) {
+    $number = (int) $connection->read();
+    if ($number > count($fileStrings) - 1) {
+        $number = $number % count($fileStrings);
+    }
+    $string = $fileStrings[$number - 1];
+    echo $number . ":" . $string . "\n";
+    $connection->write($string);
 });
+
+echo "Server starting... \n";
+
+$server->listen();
